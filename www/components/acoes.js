4be01c1e1d00832415e5703d05959852c889d14a
PauloDS98 <desenvolver.paulo98@gmail.com>
2019-10-30 03:15:21 +0900

    Project's Initial Commit

---
+++ www/components/acoes.js
@@ -0,0 +1,22 @@
+// This is a JavaScript file
+    ons.ready(function() {
+      console.log("Onsen UI is ready!");
+    });
+
+    window.fn = {};
+    window.fn.open = function() {
+      var menu = document.getElementById('menu');
+      menu.open();
+    };
+    window.fn.load = function(page) {
+      var content = document.getElementById('content');
+      var menu = document.getElementById('menu');
+      content
+        .load(page)
+        .then(menu.close.bind(menu));
+    };
+
+    if (ons.platform.isIPhoneX()) {
+      document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
+      document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
+    }
